9ccb1c5506c0abd4413afe50df44390f258439e3
Peter <dinhtruong018@gmail.com>
2019-11-28 14:42:08 +0700

    Commit

--- webapi/serializers.py
+++ webapi/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from webapi.models import *
 
+CHOICES_GENDER = (('1', 'Men'), ('0', 'Female'))
+CHOICES_RANK = (('0', 'Kem'), ('1', 'Trung Binh'), ('2', 'Kha'), ('3', 'Gioi'), ('4', 'Xuat Xac'))
 
 class CoSoDaoTaoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,15 +16,10 @@ class LoaiVanBangSerializer(serializers.ModelSerializer):
 
 
 class VanBangSerializers(serializers.ModelSerializer):
-    gioitinh = serializers.SerializerMethodField()
-    xeploai = serializers.SerializerMethodField()
+    gioitinh = serializers.ChoiceField(choices=CHOICES_GENDER, source='get_gioitinh_display')
+    xeploai = serializers.ChoiceField(choices=CHOICES_RANK, source='get_xeploai_display')
 
     class Meta:
         model = VanBang
         fields = ('id', 'hoten', 'ngaysinh', 'gioitinh', 'cmnd', 'cosodaotao', 'ngaycapbang',
             'sohieu', 'chuyennganh', 'xeploai', 'loai_vanbang')
-
-    def get_gioitinh(self,obj):
-        return obj.get_gioitinh_display()
-    def get_xeploai(self,obj):
-        return obj.get_xeploai_display()
