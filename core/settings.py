4e7078eeb51c03eab24c58ea5eb2cc964c40cac4
Peter <dinhtruong018@gmail.com>
2019-11-28 18:41:30 +0700

    Data

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 1000,
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
